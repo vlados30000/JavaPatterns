fe62a2eabaaf1827b74c0cea9662e2cf804c7675
Vlad <vladekshibarov@yandex.ru>
2024-03-05 10:39:32 +0300

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,76 @@
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String city;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return age >= 0;
+    }
+
+    public boolean hasAddress() {
+        return city != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        return city;
+    }
+
+    public String setAddress(String city) {
+        this.city = city;
+        return this.city;
+    }
+
+    public void happyBirthday() {
+        if (hasAge()) {
+            age++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "человек " + "по имени: " + name
+                + ", фамилия: " + surname
+                + ", возраст: " + age
+                + ", проживающий в городе " + city;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, city);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder().setSurname(surname).setAddress(city);
+    }
+
+}
---
+++ src/PersonBuilder.java
@@ -0,0 +1,55 @@
+public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String city;
+
+    public PersonBuilder setName(String name) throws IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя неккоретно");
+        } else {
+            this.name = name;
+            return this;
+        }
+    }
+
+    public PersonBuilder setSurname(String surname) throws IllegalArgumentException {
+        if (surname == null || surname.isEmpty()) {
+            throw new IllegalArgumentException("Фамилия неккоретна");
+        } else {
+            this.surname = surname;
+            return this;
+        }
+    }
+
+    public PersonBuilder setAge(int age) throws IllegalArgumentException {
+        if (age < 0) throw new IllegalArgumentException("Отрицательный возраст недопустим");
+        else {
+            this.age = age;
+            return this;
+        }
+    }
+
+    public PersonBuilder setAddress(String city) throws IllegalArgumentException {
+        this.city = city;
+        return this;
+
+    }
+
+    public Person build() {
+        if (name == null)
+            throw new IllegalStateException("Имя не задано");
+        if (surname == null)
+            throw new IllegalStateException("Фамилия не задана");
+        Person person;
+        if (age < 0) {
+            person = new Person(name, surname);
+        } else
+            person = new Person(name, surname, age);
+        person.setAddress(city);
+        return person;
+    }
+
+
+}
